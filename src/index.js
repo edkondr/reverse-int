d690fea5444c76fc9570fd7d7fb01d5f462d4293
edkondr <edyard2002@mail.ru>
2020-10-20 00:40:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-module.exports = function reverse (n) {
-    
-    
-        return String(Math.abs(n)).split('').reverse().join('');
+function min (array) {
+        if (!array || array.length == 0)//проверка пустой ли массив
+        return 0;
+        else{
+              return Math.min.apply(Math, array);
+        }
+      };
+      
+function max (array) {
+        if (!array || array.length == 0)//проверка пустой ли массив
+        return 0;
+        else{
+          return Math.max.apply(Math, array);
+        }
+      };
+      
+function avg (array) {
+        if (!array || array.length == 0)//проверка пустой ли массив
+        return 0;
+        else{
+          var a;
+          var b;
+              return array.reduce((a, b) => (a + b)) / array.length;
+        }
         
-    
-}
+      };
+      
